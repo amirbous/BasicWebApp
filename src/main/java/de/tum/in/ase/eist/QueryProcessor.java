910fbf0db19dde473939915d1002b5a53c0b9230
amirbous <a.amirbouslama@gmail.com>
2022-07-15 10:32:12 +0200

    query answer

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,9 +12,10 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        else if (query.contains("what is your name")){
-		    return "amir";
-	    }   else if (query.contains("name")) {
+        else if (query.contains("largest")) {
+            return "20374";
+        }
+        else if (query.contains("name")) {
            return "amir";
         } else { // TODO extend the programm here
             return "";
